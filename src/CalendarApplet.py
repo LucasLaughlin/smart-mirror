7695ed81a7a41684aeaf373d110853699e0a04dc
nicholasbvolpe <41712102+nicholasbvolpe@users.noreply.github.com>
2020-04-16 18:52:54 -0600

    Update CalendarApplet.py

--- src/CalendarApplet.py
+++ src/CalendarApplet.py
@@ -1,9 +1,5 @@
 import datetime
-import pickle
-import os.path
 from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 
 class CalendarApplet():
     creds = None
