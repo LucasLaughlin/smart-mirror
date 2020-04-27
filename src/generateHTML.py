5b92babd19eabbc422c941bfe251e36d56d6662c
Natty-Laugh <lucas_laughlin@yahoo.com>
2020-04-27 12:45:52 -0600

    file path

--- src/generateHTML.py
+++ src/generateHTML.py
@@ -1,15 +1,21 @@
 import webbrowser
+from GmailApplet import GmailApplet 
+from rss import RSSApp 
+from CalendarApplet import CalendarApplet 
 
 
 class GenerateHTML():
 
     def __init__(self):
-        self.f = open('mirrordisplay.html', 'w')
+        self.f = open('/var/www/index/index.html', 'w')
+        self.gmail = GmailApplet()
+        self.calender = CalendarApplet()
+        self.rss = RSSApp()
+        
 
     def generate(self):
         message1 = """<div> blah blah blah </div>"""
-        message = """
-            <!DOCTYPE html>
+        message = """<!DOCTYPE html>
             <html lang="en-US">
                 <head>
                     <meta charset="UTF-8">
@@ -22,71 +28,71 @@ class GenerateHTML():
                     <!-- Popper JS -->
                     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
                     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-                </head>
-                <body>
-                <div class="container">
-                    <div class="row fixed-top">
-                    """ + message1 + """
-                        <div class="col m-4"> 
-                            <ul class="list-group">
-                                <li class="list-group-item"> <h5> Gmail Notifications </h5> </li>
-                                <li name="GmailItemOne" class="list-group-item"> First </li>
-                                <li name="GmailItemTwo" class="list-group-item"> Second </li>
-                                <li name="GmailItemThree" class="list-group-item"> Third </li>
-                                <li name="GmailItemFour" class="list-group-item"> Fourth </li>
-                                <li name="GmailItemFive" class="list-group-item"> Fifth </li>
-                            </ul>
-                            <ul class="list-group">
-                                <li class="list-group-item"> <h5> Calendar Notifications </h5> </li>
-                                <li name="CalendarItemOne" class="list-group-item"> First </li>
-                                <li name="CalendarItemTwo" class="list-group-item"> Second </li>
-                                <li name="CalendarItemThree" class="list-group-item"> Third </li>
-                                <li name="CalendarItemFour" class="list-group-item"> Fourth </li>
-                                <li name="CalendarItemFive" class="list-group-item"> Fifth </li>
-                            </ul>
-                        </div>
-                        <div class="col p-0"> </div>
-                        <div class="col m-4"> 
-                            <h1 class=""> Time </h1>
-                            <div class="pl-2"> 
-                                <span class=" " name="temperature"> Degrees </p>
-                                <span class=" " name="cloudcover"> Coverage </p>
+                	<style>
+            		    body {background-color:black; color: white;}
+            		    .list-group-item {background-color:black;}
+            		    h5 {border-bottom: 5px solid white;}
+            	    </style>
+                    </head>
+                    <body>
+                    <div class="container">
+                        <div class="row fixed-top">
+                            <div class="col m-4"> 
+                                <ul class="list-group">
+                                    <li class="list-group-item"> <h5> Calendar Notifications </h5> </li>
+                                    <li name="CalendarItemOne" class="list-group-item"> First </li>
+                                    <li name="CalendarItemTwo" class="list-group-item"> Second </li>
+                                    <li name="CalendarItemThree" class="list-group-item"> Third </li>
+                                    <li name="CalendarItemFour" class="list-group-item"> Fourth </li>
+                                    <li name="CalendarItemFive" class="list-group-item"> Fifth </li>
+                                </ul>
                             </div>
-                            <h3 name="Alarm" class="pt-3"> Alarm:  </h3>
+                            <div class="col p-0"> </div>
+                            <div class="col m-4"> 
+                                <h1 class=""> 315 </h1>
+                                <div class="pl-2"> 
+                                    <span class=" " name="temperature"> Degrees: </span>
+                                    <span class=" " name="cloudcover"> Coverage: </span>
+            	                <span class=" " name="unreadEmails"> Unread Emails: </span>
+                                </div>
+                                <h3 name="Alarm" class="pt-3"> Alarm:  </h3>
+                            </div>
+                        </div>
+                        
+                        <div class="row fixed-bottom">
+                            <div class="col m-4"> 
+                                <ul class="list-group">
+                                    <li class="list-group-item"> <h5> Twitter Highlights </h5> </li>
+                                    <li name="TwitterItemOne" class="list-group-item"> RT @reeseynem: #outwest https://t.co/tA4maglajG </li>
+                                    <li name="TwitterItemTwo" class="list-group-item"> Why are men more likely to get worse symptoms and die from covid-19? https://t.co/XLOvhxJm6P https://t.co/vni5sHzQt4 </li>
+                                    <li name="TwitterItemThree" class="list-group-item"> https://t.co/1j7iyDaO1I </li>
+                                    <li name="TwitterItemFour" class="list-group-item"> Never made it as a wise man </li>
+                                    <li name="TwitterItemFive" class="list-group-item"> RT @chefjoseandres: And he did the same in Puerto Rico after Maria, and many other times! He is a great guy, friend, leader....tones of car… </li>
+                                </ul>
+            	        </div>
+            	        <div class="col p-0"> </div>
+             	        <div class="col m-4 " >
+                                <ul class="list-group">
+                                    <li class="list-group-item"> <h5> RSS </h5> </li>
+                                    <li name="RSSItemOne" class="list-group-item"> This should change </li>
+                                    <li name="RSSItemTwo" class="list-group-item"> Second </li>
+                                    <li name="RSSItemThree" class="list-group-item"> Third </li>
+                                    <li name="RSSItemFour" class="list-group-item"> Fourth </li>
+                                    <li name="RSSItemFive" class="list-group-item"> Fifth </li>
+                                </ul>
+            	    	</div>
+                            </div>
+                            <div class="col"> </div>
+                            <div class="col"> </div>
+                
                         </div>
                     </div>
-            
-                    <div class="row fixed-bottom">
-                        <div class="col m-4"> 
-                            <ul class="list-group">
-                                <li class="list-group-item"> <h5> Twitter Highlights </h5> </li>
-                                <li name="TwitterItemOne" class="list-group-item"> First </li>
-                                <li name="TwitterItemTwo" class="list-group-item"> Second </li>
-                                <li name="TwitterItemThree" class="list-group-item"> Third </li>
-                                <li name="TwitterItemFour" class="list-group-item"> Fourth </li>
-                                <li name="TwitterItemFive" class="list-group-item"> Fifth </li>
-                            </ul>
-                            <ul class="list-group">
-                                <li class="list-group-item"> <h5> RSS </h5> </li>
-                                <li name="RSSItemOne" class="list-group-item"> First </li>
-                                <li name="RSSItemTwo" class="list-group-item"> Second </li>
-                                <li name="RSSItemThree" class="list-group-item"> Third </li>
-                                <li name="RSSItemFour" class="list-group-item"> Fourth </li>
-                                <li name="RSSItemFive" class="list-group-item"> Fifth </li>
-                            </ul>
-            
-                        </div>
-                        <div class="col"> </div>
-                        <div class="col"> </div>
-            
-                    </div>
-                </div>
-                </body>
-            </html>"""
-        
+                    </body>
+                </html>"""
+
         self.f.write(message)
         self.f.close()
-        webbrowser.open_new_tab('mirrordisplay.html')
+        webbrowser.open_new_tab('/var/www/index/index.html')
 
 
 def main():
