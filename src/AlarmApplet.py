6a7d8a40ffa2b7fe3ae48eb94448f4753060a2ac
Nicholas Volpe <nivo2681@colorado.edu>
2020-04-29 12:52:28 -0700

    Created main loop and earlier/later functions for the alarm

--- src/AlarmApplet.py
+++ src/AlarmApplet.py
@@ -8,17 +8,45 @@ class AlarmApplet(ClockApplet):
         self.alarmHour = 12
         self.alarmOn = False
         self.alarmTriggered = False
-        
+
+    def toggleState(self):
+        # Switches the state of the alarm, used
+        # when the button is pressed
+        if self.alarmOn:
+            self.alarmOn = 0
+        if not self.alarmOn:
+            self.alarmOn = 1
+
     def checkAlarmTime(self) -> bool:
+        # Checks whether the time is equal to the alarm time
         return (self.alarmMin==self.curMin) and (self.alarmHour==self.curHour)
-    
+
+    def earlier(self) -> None:
+        # Reduces the time of the alarm by fifteen minutes
+        theTime = datetime(self.year, self.month, self.day, hour=self.alarmHour, minute=self.alarmMin)
+        newTime = theTime - datetime.timedelta(minutes=15)
+        self.alarmHour = newTime.datetime.hour
+        self.alarmMinute = newTime.datetime.minute
+
+    def later(self) -> None:
+        # Increases the alarm time by fifteen minutes
+        theTime = datetime(self.year, self.month, self.day, hour=self.alarmHour, minute=self.alarmMin)
+        newTime = theTime + datetime.timedelta(minutes=15)
+        self.alarmHour = newTime.datetime.hour
+        self.alarmMinute = newTime.datetime.minute
+
+    # Override of ClockApplet
     def getData(self) -> None:
+        # Uses ClockApplet's getData() to retrieve the time
         super().getData()
-        if ( self.alarmOn and self.checkAlarmTime()): 
+        # Checks to see if the alarm state is active and the
+        # alarm time is equal to the current time
+        if ( self.alarmOn and self.checkAlarmTime()):
             self.alarmTriggered = True
         else:
             self.alarmTriggered = False
-    
+
+    # Override of ClockApplet
     def display(self) -> None:
         self.getData()
         if self.alarmTriggered: color = 'red'
--- src/main.py
+++ src/main.py
@@ -1,11 +1,40 @@
-from CalendarApplet import CalendarApplet
-from  GmailApplet import GmailApplet
+from AlarmApplet import AlarmApplet
+from generateHTML import GenerateHTML
+import time
+import pygame
 
 def main():
-    gmailApp = GmailApplet()
-    calenderApp = CalendarApplet()
-    gmailApp.getData()
-    calenderApp.getData()
+    # https://stackoverflow.com/questions/24352768/python-key-press-and-key-release-listener
+    # Pygame has a simpler solution to waiting for user input than originally used
+    pygame.init()
+    # This statement triggers a KEYDOWN event when a key is pressed
+    pygame.key.set_repeat(100, 100)
+
+    concreteAlarm = AlarmApplet()
+
+    viewGenerator = GenerateHTML()
+
+    while 1:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+
+                # If key pressed is 'a'
+                if event.key == pygame.K_a:
+                    # Toggles whether the alarm is active
+                    concreteAlarm.toggleState()
+
+                # If key pressed is 's'
+                if event.key == pygame.K_s:
+                    # Decrements alarm time by fifteen minutes
+                    concreteAlarm.earlier()
+
+                # If key pressed is 'd'
+                if event.key == pygame.K_d:
+                    # Increments alarm time by fiteen minutes
+                    concreteAlarm.later()
+
+        viewGenerator.generateHTML()
+
 
 if __name__ == '__main__':
     main()
