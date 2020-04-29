e8206d2b5795ff74a38ce09b83ebfe8f90e5f6c6
Natty-Laugh <lucas_laughlin@yahoo.com>
2020-04-29 12:39:10 -0600

    AlarmApplet

---
+++ src/AlarmApplet.py
@@ -0,0 +1,27 @@
+import datetime
+from ClockApplet import ClockApplet
+
+class AlarmApplet(ClockApplet):
+    def __init__(self):
+        super()
+        self.alarmMin = 0
+        self.alarmHour = 12
+        self.alarmOn = False
+        self.alarmTriggered = False
+        
+    def checkAlarmTime(self) -> bool:
+        return (self.alarmMin==self.curMin) and (self.alarmHour==self.curHour)
+    
+    def getData(self) -> None:
+        super().getData()
+        if ( self.alarmOn and self.checkAlarmTime()): 
+            self.alarmTriggered = True
+        else:
+            self.alarmTriggered = False
+    
+    def display(self) -> None:
+        self.getData()
+        if self.alarmTriggered: color = 'red'
+        else: color = 'whhite'
+        html = """<h3 styl="color: %s> """%color + str(self.alarmHour)+ ":"+ str(self.alarmMin) + """ </h3>"""
+        return html
---
+++ src/ClockApplet.py
@@ -0,0 +1,28 @@
+import datetime
+import subprocess
+
+class ClockApplet:
+    def __init__(self):
+        self.curMin = None
+        self.curHour = None
+        self.curDay = None
+        self.curMonth = None
+        self.curYear = None
+        
+    def getData(self) -> None:
+        # Gets current time and date
+        timeNow = datetime.datetime.now()
+        self.curHour = timeNow.hour
+        self.curMin = timeNow.minute
+        today = datetime.datetime.today()
+        self.curDay = today.day
+        self.curMonth = today.month
+        self.curYear = today.year
+    
+    def display(self) -> None:
+        self.update()
+        # Updates the HTML
+        html = """<div class="row"><h1> """ + str(self.curHour)+ ":"+ str(self.curMin) + """ </h1>
+                    <span class="pl-2 pt-1" >""" + str(self.curMonth) + "/" + str(self.curDay) + "/" + str(self.curYear) +""" </span> </div>""" 
+        return html
+
