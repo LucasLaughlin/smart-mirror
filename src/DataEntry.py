caf2ee5dc28d7be723dfb605bdc119901b39e9ce
Nicholas Volpe <nivo2681@colorado.edu>
2020-04-29 17:42:19 -0700

    Fixed RSS compatability with ListApplet

--- src/DataEntry.py
+++ src/DataEntry.py
@@ -2,9 +2,9 @@ class DataEntry():
     def __init__(self, content, date = None):
         self.content=content
         self.date =date
-        
+
     def display(self) ->str:
         if self.date: displayString = str(self.date) +" : "+ self.content
         else: displayString = self.content
         return """<li name="CalendarItemFive" class="list-group-item"> """ + displayString + """ </li>"""
-        
+
--- src/rss.py
+++ src/rss.py
@@ -5,19 +5,12 @@ from ListApplet import ListApplet
 from DataEntry import DataEntry
 
 
-class RSSPost:
-    _title: str = ""
-
-    def __init__(self, postTitle: str) -> None:
-        self._title = postTitle
-
-
 class RSSApplet(ListApplet):
     def __init__(self):
         super()
         # The RSS url. getData could be altered to accept a list of RSS urls (expanded feature set)
         self._feedURL = "https://hnrss.org/show?points=200&comments=20"
-        self.name = "RSS Feed"
+        self.title = "RSS Feed"
         self.entries = list()
 
     # Currently handles one RSS feed
@@ -31,9 +24,9 @@ class RSSApplet(ListApplet):
             # Pull out the important info to display
             # Selects top(latest) 5 posts that fit query criteria
             for index in range(5):
-                post1 = RSSPost(HNFeed['entries'][index]['title'])
+                post1 = HNFeed['entries'][index]['title']
 
                 # Add post to list of posts that will be displayed
-                self.entries.append(DatatEntry(str(post1)))
+                self.entries.append(DataEntry(post1))
         else:
             print("RSS app has recieved invalid XML")
